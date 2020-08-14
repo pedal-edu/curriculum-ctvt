44d8b767aacc94353918d62bc7fdbf98c07c7ac9
lukesg08 <lukesg08@vt.edu>
2020-08-13 23:01:55 -0400

    Adding a custom plotting function for the ct curriculum

---
+++ curriculum_ctvt/plotting.py
@@ -0,0 +1,55 @@
+from pedal import Feedback, CompositeFeedbackFunction
+from pedal.assertions import ensure_function_call, prevent_function_call
+from pedal.core.feedback import FeedbackResponse
+from pedal.core.report import MAIN_REPORT
+from pedal.types.builtin import BUILTIN_MODULES
+from pedal.types.definitions import ModuleType, FunctionType, NoneType
+from pedal.cait.find_node import function_is_called
+from pedal.cait.cait_api import parse_program, def_use_error
+from pedal.sandbox.commands import get_sandbox
+from pedal.extensions.plotting import other_plt, wrong_plt_data, wrong_plt_type, no_plt, GRAPH_TYPES, compare_data
+from collections import namedtuple
+
+
+@CompositeFeedbackFunction(other_plt, wrong_plt_data, wrong_plt_type, no_plt)
+def assert_one_of_plots(plt_type, data_plots, **kwargs):
+    """
+    Check whether a plot with the given ``plt_type`` and one of``data`` exists.
+    If the plot was found successfully, returns False.
+    Otherwise, returns the feedback that was detected.
+
+    Args:
+        plt_type (str): Either 'line', 'hist', or 'scatter'
+        data (list): The expected data to check in the plots in osurce code. Each item in the
+                    list should either be a list of numbers or a tuple of two lists (for scatter/line plots)
+    """
+    report = kwargs.get("report", MAIN_REPORT)
+    # Allow instructor to use "plot" instead of "line" as type
+    if plt_type == 'plot':
+        plt_type = 'line'
+    # Check the plots to see if there is a plot with the data
+    type_found = False
+    data_found = False
+    plots = get_sandbox(report=report).modules.plotting.plots
+    for graph in plots:
+        for a_plot in graph['data']:
+            data_found_here = False
+            for data in data_plots:
+                data_found_here = compare_data(plt_type, data, a_plot)
+            if a_plot['type'] == plt_type and data_found_here:
+                return False
+            if a_plot['type'] == plt_type:
+                type_found = True
+            if data_found_here:
+                data_found = data_found_here
+    # Figure out what kind of mistake was made.
+    data = data_plots[0]
+    plt_type = GRAPH_TYPES.get(plt_type, plt_type)
+    if type_found and data_found:
+        return other_plt(plt_type, data, data_found)
+    elif type_found:
+        return wrong_plt_data(plt_type, data, data_found)
+    elif data_found:
+        return wrong_plt_type(plt_type, data, data_found)
+    else:
+        return no_plt(plt_type, data, data_found)
---
+++ tests/test_plotting.py
@@ -0,0 +1,153 @@
+import unittest
+from textwrap import dedent
+
+from pedal.core import *
+from pedal.core.report import MAIN_REPORT
+from pedal.core.commands import clear_report, suppress, contextualize_report
+from pedal.sandbox.commands import get_sandbox
+from pedal.source import verify
+from pedal.tifa import tifa_analysis
+from pedal.resolvers import simple
+from pedal.sandbox import commands
+from pedal.cait.cait_api import parse_program
+from curriculum_ctvt.plotting import assert_one_of_plots
+
+code_hist_and_plot = ('''
+import matplotlib.pyplot as plt
+plt.hist([1,2,3])
+plt.title("My line plot")
+plt.show()
+plt.plot([4,5,6])
+plt.show()''')[1:]
+
+
+class Execution:
+    """
+
+    """
+    def __init__(self, code, tracer_style='none', old_style_messages=False,
+                 run_tifa=True, report=MAIN_REPORT):
+        self.code = code
+        self.tracer_style = tracer_style
+        self.old_style_messages = old_style_messages
+        self.report=report
+        self.final = None
+        self.run_tifa = run_tifa
+
+    def __enter__(self):
+        clear_report(report=self.report)
+        contextualize_report(self.code, report=self.report)
+        verify(report=self.report)
+        if self.run_tifa:
+            tifa_analysis(report=self.report)
+        # TODO: Clean this up
+        self.student = get_sandbox(self.report)
+        self.student.report_exceptions_mode = True
+        self.report['sandbox']['sandbox'].tracer_style = self.tracer_style
+        commands.run()
+        return self
+
+    def __exit__(self, *args):
+        suppress("runtime", "FileNotFoundError", report=self.report)
+        self.final = simple.resolve()
+        self.feedback = """{title}\n{message}""".format(
+            title=self.final.title,
+            message=self.final.message
+        )
+
+
+class ExecutionTestCase(unittest.TestCase):
+    def assertFeedback(self, execution, feedback_string):
+        """
+
+        Args:
+            execution:
+            feedback_string:
+
+        Returns:
+
+        """
+        return self.assertEqual(dedent(feedback_string).lstrip(), execution.feedback)
+
+    def test_check_for_plot_correct_hist(self):
+        with Execution(code_hist_and_plot) as e:
+            self.assertEqual(assert_one_of_plots('hist', [[1, 2, 3]]), False)
+        self.assertFeedback(e, "No Errors\nNo errors reported.")
+
+    def test_check_for_plot_wrong_hist(self):
+        with Execution(code_hist_and_plot) as e:
+            assert_one_of_plots('hist', [[1, 2, 3, 4]])
+        self.assertFeedback(e, "Plot Data Incorrect\n"
+                               "You have created a histogram, but it does not "
+                               "have the right data.")
+
+    def test_check_for_plot_correct_plot(self):
+        with Execution(code_hist_and_plot) as e:
+            self.assertEqual(assert_one_of_plots('line', [[4, 5, 6]]), False)
+        self.assertFeedback(e, "No Errors\nNo errors reported.")
+
+    def test_check_for_plot_wrong_plot(self):
+        with Execution(code_hist_and_plot) as e:
+            assert_one_of_plots('line', [[4, 5, 6, 7]])
+        self.assertFeedback(e, "Plot Data Incorrect\n"
+                               "You have created a line plot, but it does not "
+                               "have the right data.")
+
+    def test_assert_plot_wrong_type_of_plot(self):
+        student_code = dedent('''
+            import matplotlib.pyplot as plt
+            plt.plot([1,2,3])
+            plt.title("My line plot")
+            plt.show()
+        ''')
+        with Execution(student_code) as e:
+            assert_one_of_plots('hist', [[1, 2, 3]])
+        self.assertFeedback(e, "Wrong Plot Type\n"
+                               "You have plotted the right data, but you appear "
+                                "to have not plotted it as a histogram.")
+
+    def test_assert_plot_wrong_data_place(self):
+        student_code = dedent('''
+            import matplotlib.pyplot as plt
+            plt.plot([1,2,3])
+            plt.title("Wrong graph with the right data")
+            plt.show()
+            plt.hist([4,5,6])
+            plt.title("Right graph with the wrong data")
+            plt.show()
+        ''')
+        with Execution(student_code) as e:
+            assert_one_of_plots('hist', [[1, 2, 3]])
+        self.assertFeedback(e, "Plotting Another Graph\n"
+                               "You have created a histogram, but it does not "
+                               "have the right data. That data appears to have "
+                               "been plotted in another graph.")
+
+        student_code = dedent('''
+            import matplotlib.pyplot as plt
+            plt.plot([1,2,3])
+            plt.title("My line plot")
+            plt.show()
+        ''')
+        with Execution(student_code) as e:
+            self.assertEqual(assert_one_of_plots('hist', [[4, 5, 6]]),
+                             "You have not created a histogram with the "
+                             "proper data.<br><br><i>(no_plt)<i></br></br>")
+
+        student_code = dedent('''
+            import matplotlib.pyplot as plt
+            plt.scatter([], [])
+            plt.title("Nothingness and despair")
+            plt.show()
+        ''')
+        with Execution(student_code) as e:
+            self.assertEqual(assert_one_of_plots('scatter', [[]]), False)
+
+        student_code = dedent('''
+            import matplotlib.pyplot as plt
+            plt.scatter([1,2,3], [4,5,6])
+            plt.title("Some actual stuff")
+            plt.show()
+        ''')
+        with Execution(student_code) as e:
+            assert_one_of_plots('scatter', [([1, 2, 3], [4, 5, 6])])
