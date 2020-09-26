82fe54069797f3fa38c139e1532e448479e9f9b5
Austin Cory Bart <acbart@vt.edu>
2020-09-26 14:45:53 -0400

    Added a setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name='curriculum_ctvt',
+    version='1.0.0',
+    python_requires='>=3.6',
+    author='lukesg',
+    author_email='lukesg08@vt.edu',
+    description='Pedal Feedback Functions for the CTVT Curriculum',
+    keywords='feedback education teaching program analysis tifa cait sandbox pedal grading grader grade',
+    packages=['curriculum_ctvt'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+    install_requires=[],
+    url='https://pedal-edu.github.io/pedal',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Education",
+        "Topic :: Education",
+    ]
+)
